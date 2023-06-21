2e8eaa078c61adbacdeb45bbe69676f75f9890dd
Roman Dubrovin <dubrovin.skfo@gmail.com>
2023-06-21 17:13:08 +0300

    сделал кучу хрени

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,1 @@
 "use strict";
-
-const btn = document.querySelector(".btn");
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const h1 = document.querySelector(".h1");
-
-btn.addEventListener("click", () => {
-  h1.insertAdjacentHTML("afterend", "<h2>Вы нажали кнопку</h2>");
-});
-
-btn1.addEventListener("click", () => {
-  h1.style.color = "red";
-});
-
-btn2.addEventListener("click", () => (h1.style.color = "yellow"));
