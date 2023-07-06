a7a8524657c2e16567238316cd1406e3f64bf06f
Roman Dubrovin <dubrovin.skfo@gmail.com>
2023-07-06 17:40:36 +0300

    add event to scroll btn

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const page = {
+  clientsLogoSec: {
+    scrollBtn: document.querySelector(".clients-logo__scroll-button"),
+  },
+  liveAuctionsSection: document.querySelector(".live-auctions-section"),
+  liveAuctionsSec: {
+    titleSecond: document.querySelector(".live-auctions-section__title-second"),
+  },
+};
+
+//прокрутка при нажатии scrollBtn
+page.clientsLogoSec.scrollBtn.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  page.liveAuctionsSec.titleSecond.scrollIntoView({ behavior: "smooth" });
+});
