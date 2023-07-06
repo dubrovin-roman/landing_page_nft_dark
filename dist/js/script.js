de5cf57d065f4614208ae0826e86983c1506e905
Roman <dubrovin.skfo@gmail.com>
2023-07-06 20:49:37 +0300

    всплытие секций

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const page = {
+  sections: document.querySelectorAll(".section"),
   clientsLogoSec: {
     scrollBtn: document.querySelector(".clients-logo__scroll-button"),
   },
@@ -10,8 +11,26 @@ const page = {
   },
 };
 
-//прокрутка при нажатии scrollBtn
+// прокрутка при нажатии scrollBtn
 page.clientsLogoSec.scrollBtn.addEventListener("click", (ev) => {
   ev.preventDefault();
   page.liveAuctionsSec.titleSecond.scrollIntoView({ behavior: "smooth" });
+});
+
+// всплытие секций при прокрутке
+const sectionObserver = new IntersectionObserver(
+  (entries, observer) => {
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.remove("section-hidden");
+      observer.unobserve(entries[0].target);
+    }
+  },
+  {
+    threshold: 0.15,
+  }
+);
+
+page.sections.forEach((elem) => {
+  elem.classList.add("section-hidden");
+  sectionObserver.observe(elem);
 });
